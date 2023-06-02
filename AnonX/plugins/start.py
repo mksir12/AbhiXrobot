af38a2fe4744065618aeec24ba4bfffd3fc08f40
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-06-03 00:44:55 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgUAAxkBAAIHP2R6FV6shZxSK4-djr7W0ZVLdNEZAAIGAwACNZnYVOTUhlBiHTQGLwQ")
+                await message.reply_sticker("CAACAgUAAxkBAAIHhWR6P0doAcBNnaPeIoL8-UiSkAypAAL_CAACYyWoV2UBta6PIrT1LwQ")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
